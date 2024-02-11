8775f917fbe0913bd25bbc82cf311611f177e24b
Filip Dupanović <filip.dupanovic@gmail.com>
2024-02-11 15:07:46 +0000

    feat(github): Add `full_name`

--- modules/github/variables.tf
+++ modules/github/variables.tf
@@ -1,3 +1,9 @@
+variable "full_name" {
+  description = "Full name of the repository (in `org/name` format)."
+  default     = null
+  type        = string
+}
+
 variable "name" {
   description = "The name of the repository."
   default     = null
