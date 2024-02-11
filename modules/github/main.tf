496426c6834b754eb932d175b533690f323b013c
Filip Dupanović <filip.dupanovic@gmail.com>
2024-02-11 15:45:07 +0000

    feat(github): Configure WIF

--- modules/github/main.tf
+++ modules/github/main.tf
@@ -13,3 +13,22 @@ resource "google_service_account" "github_actions" {
   display_name = "GitHub Actions (${data.github_repository.default.full_name})"
   project      = var.project
 }
+
+resource "random_id" "pool_id" {
+  byte_length = 8
+}
+
+module "github_actions_workload_identity_federation" {
+  source  = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+  version = ">= 3.1.0"
+
+  project_id  = var.project
+  pool_id     = "github-actions-${random_id.pool_id.hex}"
+  provider_id = "github-provider-${random_id.pool_id.hex}"
+  sa_mapping = {
+    "github-actions" = {
+      sa_name   = google_service_account.github_actions.id
+      attribute = "attribute.repository/${data.github_repository.default.full_name}"
+    }
+  }
+}
--- modules/github/outputs.tf
+++ modules/github/outputs.tf
@@ -2,3 +2,8 @@ output "service_account" {
   value       = google_service_account.github_actions
   description = "GitHub Actions service account."
 }
+
+output "workload_identity_provider" {
+  value       = module.github_actions_workload_identity_federation.provider_name
+  description = "GitHub Actions workload identity provider."
+}
