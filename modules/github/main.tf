ee2d4e51feeb5e059150377d3e176fd1ba85f677
Filip Dupanović <filip.dupanovic@gmail.com>
2024-02-11 15:43:21 +0000

    feat(github): Create service account

--- modules/github/main.tf
+++ modules/github/main.tf
@@ -1,4 +1,15 @@
+locals {
+  id = "${replace(replace(data.github_repository.default, "_", "-"), "/", "-")}-gha"
+}
+
+
 data "github_repository" "default" {
   name      = var.name
   full_name = var.full_name
+}
+
+resource "google_service_account" "github_actions" {
+  account_id   = local.id
+  display_name = "GitHub Actions (${data.github_repository.default.full_name})"
+  project      = var.project
 }
