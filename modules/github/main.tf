e2b4de87ee6cdf17d0ca2f75400dd26c816acbb5
Filip Dupanović <filip.dupanovic@gmail.com>
2024-02-12 09:24:03 +0000

    feat(github): Add support for configuring deploy keys
    Adds support for managing GitHub repositories for automation.

--- modules/github/main.tf
+++ modules/github/main.tf
@@ -1,5 +1,5 @@
 locals {
-  id = "${replace(replace(data.github_repository.default, "_", "-"), "/", "-")}-gha"
+  id = "${replace(replace(replace(data.github_repository.default.full_name, ".", "-"), "_", "-"), "/", "--")}-gha"
 
   actions_variables = merge(var.actions_variables, {
     service_account            = google_service_account.github_actions.email
@@ -54,4 +54,24 @@ module "github_actions_workload_identity_federation" {
       attribute = "attribute.repository/${data.github_repository.default.full_name}"
     }
   }
+}
+
+data "github_repository" "deploy_key" {
+  for_each = var.deploy_key
+
+  full_name = each.key
+}
+
+resource "tls_private_key" "deploy_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "github_repository_deploy_key" "deploy_key" {
+  for_each = var.deploy_key
+
+  repository = data.github_repository.deploy_key[each.key].name
+  title      = "${data.github_repository.default.full_name} deploy key"
+  key        = tls_private_key.deploy_key.public_key_openssh
+  read_only  = each.value.read_only
 }
--- modules/github/variables.tf
+++ modules/github/variables.tf
@@ -8,6 +8,14 @@ variable "actions_secrets" {
   description = "Repository environment secrets to set for GitHub Actions."
 }
 
+variable "deploy_key" {
+  type = map(object({
+    read_only = bool
+  }))
+  default     = {}
+  description = "Map of GitHub repositories to add a deploy key to."
+}
+
 variable "full_name" {
   description = "Full name of the repository (in `org/name` format)."
   default     = null
